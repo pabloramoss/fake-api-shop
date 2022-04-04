aa45ce95fa0c1a49bd7c4afc43c7080819a535b6
Pablo Ramos <pablo-362@hotmail.com>
2022-04-04 10:31:47 -0300

    add favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @next/next/no-title-in-document-head */
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Simple api store</title>
+        <meta name="Simple api store" content="Api for e-commerce prototype" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
