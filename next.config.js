9d7016a267334da52a9fd27b1345cc57e2ac6f92
pabloramoss <pablo-362@hotmail.com>
2023-07-18 11:24:28 +0200

    fix: next config

--- next.config.js
+++ next.config.js
@@ -5,10 +5,15 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://simpleapistore.vercel.app/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://simpleapistore.vercel.app' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, DELETE, PATCH, POST, PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
       },
-    ]
-  }
+    ];
+  },
 }
 
 module.exports = nextConfig
