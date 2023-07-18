eef9541a86a0a6fe1500cfadf7674bff1130bb91
pabloramoss <pablo-362@hotmail.com>
2023-07-18 11:19:02 +0200

    fix: cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://simpleapistore.vercel.app/:path*',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
